0a90130e1b7056da0d9849dd2fb12d662ef179ad
Ignacio Deandreis Denis <ignacio@astropay.com>
2021-04-07 16:37:39 -0300

    [wip] home

---
+++ app/src/main/java/com/test/androiddevelopersexample/ui/fragments/home/MoneyFragment.kt
@@ -0,0 +1,24 @@
+package com.test.androiddevelopersexample.ui.fragments.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.test.androiddevelopersexample.R
+import com.test.androiddevelopersexample.ui.fragments.base.FragmentBase
+
+/**
+ * Created by ignaciodeandreisdenis on 4/7/21.
+ */
+class MoneyFragment : FragmentBase() {
+
+    override var screenTag = "MoneyFragment"
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_more, container, false)
+    }
+}
