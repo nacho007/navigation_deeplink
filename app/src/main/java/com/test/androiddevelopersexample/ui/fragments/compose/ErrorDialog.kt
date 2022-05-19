6b7d7306a57fad4355173e4f50a57c4492e6cb40
Ignacio Deandreis Denis <ignacio@astropay.com>
2022-05-19 18:41:54 -0300

    [refactor] created custom dialog similar to alert dialog

--- app/src/main/java/com/test/androiddevelopersexample/ui/fragments/compose/ErrorDialog.kt
+++ app/src/main/java/com/test/androiddevelopersexample/ui/fragments/compose/ErrorDialog.kt
@@ -1,12 +1,35 @@
 package com.test.androiddevelopersexample.ui.fragments.compose
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.test.androiddevelopersexample.R
 
@@ -31,6 +54,16 @@ fun ErrorDialog(
                 )
             }
         },
+        dismissButton = {
+            TextButton(onClick = onConfirm)
+            {
+                Text(
+                    text = stringResource(id = R.string.mobile_cancel),
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colors.primary
+                )
+            }
+        },
         title = {
             Text(
                 text = stringResource(id = R.string.mobile_error),
@@ -45,5 +78,80 @@ fun ErrorDialog(
                 color = MaterialTheme.colors.onSurface,
             )
         }
+    )
+}
+
+@Preview
+@Composable
+fun ShowPreview() {
+    ErrorDialog(
+        onConfirm = {},
+        desc = "My description"
+    )
+}
+
+@Composable
+fun CustomDialog(onConfirm: () -> Unit, onCancel: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .size(320.dp, 160.dp)
+            .background(colorResource(R.color.color_balloon), RoundedCornerShape(4.dp))
+    ) {
+
+        Text(
+            modifier = Modifier
+                .padding(top = 24.dp, start = 24.dp, end = 24.dp)
+                .fillMaxWidth(),
+            text = stringResource(id = R.string.mobile_error),
+            color = MaterialTheme.colors.onSurface,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+
+        Text(
+            modifier = Modifier
+                .padding(top = 16.dp, start = 24.dp, end = 24.dp)
+                .fillMaxWidth(),
+            text = "My description",
+            fontSize = 16.sp,
+            color = MaterialTheme.colors.onSurface
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(horizontal = 8.dp, vertical = 4.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            TextButton(onClick = onCancel)
+            {
+                Text(
+                    text = stringResource(id = R.string.mobile_cancel),
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colors.primary
+                )
+            }
+
+            TextButton(onClick = onConfirm)
+            {
+                Text(
+                    text = stringResource(id = R.string.mobile_ok),
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colors.primary
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ShowPreview2() {
+    CustomDialog(
+        onConfirm = {},
+        onCancel = {}
     )
 }
--- app/src/main/java/com/test/androiddevelopersexample/ui/fragments/more/MoreFragment.kt
+++ app/src/main/java/com/test/androiddevelopersexample/ui/fragments/more/MoreFragment.kt
@@ -3,20 +3,11 @@ package com.test.androiddevelopersexample.ui.fragments.more
 import android.content.Context
 import android.os.Bundle
 import android.view.View
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,12 +15,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.fragment.findNavController
 import com.google.gson.Gson
@@ -40,6 +27,8 @@ import com.test.androiddevelopersexample.constants.FIREBASE_TOKEN
 import com.test.androiddevelopersexample.databinding.FragmentMoreBinding
 import com.test.androiddevelopersexample.ui.activities.NavigationActivity
 import com.test.androiddevelopersexample.ui.fragments.base.BaseFragment
+import com.test.androiddevelopersexample.ui.fragments.compose.CustomDialog
+import com.test.androiddevelopersexample.ui.fragments.compose.ErrorDialog
 import com.test.androiddevelopersexample.ui.fragments.compose.ModalTransitionDialog
 import com.test.androiddevelopersexample.ui.fragments.custom.ComposeIconButton
 import com.test.androiddevelopersexample.ui.utils.DeepLinkUtils.PUSH_LOYALTY
@@ -72,11 +61,21 @@ class MoreFragment : BaseFragment<FragmentMoreBinding>(FragmentMoreBinding::infl
                         item {
                             Spacer(modifier = Modifier.height(16.dp))
 
-                            var visible by rememberSaveable { mutableStateOf(false) }
-                            ShowDialogWithAnimationButton(onClick = {
-                                visible = true
+                            var visibleAlertDialog by rememberSaveable { mutableStateOf(false) }
+                            var visibleCustomDialog by rememberSaveable { mutableStateOf(false) }
+
+                            ShowAlertDialogButton(onClick = {
+                                visibleAlertDialog = true
                             })
-                            ShowErrorDialog(visible) { visible = false }
+
+                            ShowCustomDialogButton(onClick = {
+                                visibleCustomDialog = true
+                            })
+
+                            ShowAlertErrorDialog(visibleAlertDialog) { visibleAlertDialog = false }
+                            ShowCustomErrorDialog(visibleCustomDialog) {
+                                visibleCustomDialog = false
+                            }
                             CustomComponentButton()
                             CoilButton()
                             BadgesButton()
@@ -98,58 +97,34 @@ class MoreFragment : BaseFragment<FragmentMoreBinding>(FragmentMoreBinding::infl
     }
 
     @Composable
-    private fun ShowDialogWithAnimationButton(onClick: () -> Unit) {
-        ComposeIconButton(text = stringResource(id = R.string.show_dialog), action = {
+    private fun ShowCustomDialogButton(onClick: () -> Unit) {
+        ComposeIconButton(text = stringResource(id = R.string.show_custom_dialog), action = {
             onClick()
         })
     }
 
     @Composable
-    private fun ShowErrorDialog(visible: Boolean, onClick: () -> Unit) {
+    private fun ShowAlertDialogButton(onClick: () -> Unit) {
+        ComposeIconButton(text = stringResource(id = R.string.show_alert_dialog), action = {
+            onClick()
+        })
+    }
+
+    @Composable
+    private fun ShowAlertErrorDialog(visible: Boolean, onClick: () -> Unit) {
+        if (visible) {
+            ErrorDialog(onConfirm = { onClick() }, desc = "My description")
+        }
+    }
+
+    @Composable
+    private fun ShowCustomErrorDialog(visible: Boolean, onClick: () -> Unit) {
         if (visible) {
             ModalTransitionDialog(onDismissRequest = { onClick() }) { modalTransitionDialogHelper ->
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.White)
-                ) {
-
-                    Button(
-                        modifier = Modifier.align(Alignment.End),
-                        onClick = modalTransitionDialogHelper::triggerAnimatedClose
-                    ) {
-                        Text(
-                            text = "Close"
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.size(32.dp))
-
-                    Text(
-                        modifier = Modifier
-                            .padding(16.dp)
-                            .fillMaxWidth(),
-                        textAlign = TextAlign.Center,
-                        text = "Modal Transition Dialog"
-                    )
-
-                    Box(modifier = Modifier.fillMaxSize()) {
-                        Button(
-                            modifier = Modifier
-                                .padding(horizontal = 16.dp)
-                                .fillMaxWidth()
-                                .align(Alignment.Center),
-                            colors = ButtonDefaults.buttonColors(
-                                backgroundColor = colorResource(
-                                    id = R.color.color_black
-                                )
-                            ),
-                            onClick = modalTransitionDialogHelper::triggerAnimatedClose
-                        ) {
-                            Text(text = "Close it", color = Color.White)
-                        }
-                    }
-                }
+                CustomDialog(
+                    onConfirm = modalTransitionDialogHelper::triggerAnimatedClose,
+                    onCancel = modalTransitionDialogHelper::triggerAnimatedClose
+                )
             }
         }
     }
