5bfdcb340d6b5b99e7245deca3598a679a411eac
Ignacio Deandreis Denis <ignacio@astropay.com>
2022-06-02 13:17:22 -0300

    [wip] material

--- app/src/main/java/com/test/androiddevelopersexample/ui/custom/CustomCountry.kt
+++ app/src/main/java/com/test/androiddevelopersexample/ui/custom/CustomCountry.kt
@@ -3,7 +3,9 @@ package com.test.androiddevelopersexample.ui.custom
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.card.MaterialCardView
 import com.test.androiddevelopersexample.databinding.CustomCountryItemViewBinding
 import com.test.androiddevelopersexample.ui.fragments.swipe.Country
 import com.test.androiddevelopersexample.ui.utils.show
@@ -15,13 +17,16 @@ class CustomCountry @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = 0
-) : ConstraintLayout(context, attrs, defStyle) {
+) : MaterialCardView(context, attrs, defStyle) {
 
     private var binding: CustomCountryItemViewBinding =
         CustomCountryItemViewBinding.inflate(LayoutInflater.from(context), this, true)
 
     init {
-
+        binding.root.setOnClickListener {
+            super.callOnClick()
+            Toast.makeText(context, "Custom Pressed", Toast.LENGTH_SHORT).show()
+        }
     }
 
     fun setCountry(country: Country) {
