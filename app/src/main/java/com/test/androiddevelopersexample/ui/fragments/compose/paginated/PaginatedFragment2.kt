c7b24ec5e8aa080c85ed9f3527008381daa1fd9d
Ignacio Deandreis <ignacio@astropay.com>
2022-07-20 16:27:41 -0300

    [wip] paging

--- app/src/main/java/com/test/androiddevelopersexample/ui/fragments/compose/paginated/PaginatedFragment2.kt
+++ app/src/main/java/com/test/androiddevelopersexample/ui/fragments/compose/paginated/PaginatedFragment2.kt
@@ -1,10 +1,10 @@
 package com.test.androiddevelopersexample.ui.fragments.compose.paginated
 
 import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -20,7 +19,6 @@ import androidx.compose.material.ContentAlpha
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.MaterialTheme.typography
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -28,10 +26,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.paging.LoadState
@@ -39,10 +38,11 @@ import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
-import com.google.accompanist.coil.rememberCoilPainter
 import com.test.androiddevelopersexample.databinding.FragmentComposeBinding
 import com.test.androiddevelopersexample.domain.User
 import com.test.androiddevelopersexample.theme.AstroPayTheme
+import com.test.androiddevelopersexample.theme.cardBackground
+import com.test.androiddevelopersexample.theme.dialogBackgroundColor
 import com.test.androiddevelopersexample.ui.fragments.base.BaseFragment
 import kotlinx.coroutines.flow.Flow
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -77,37 +77,19 @@ class PaginatedFragment2 :
                 )
                 .fillMaxWidth()
                 .clickable(onClick = onClick),
+            backgroundColor = Color.Green,
+            contentColor = Color.Red,
             shape = RoundedCornerShape(15.dp),
             elevation = 12.dp
         ) {
             Row(
                 modifier = Modifier
+                    .height(350.dp)
                     .clip(RoundedCornerShape(4.dp))
-                    .background(MaterialTheme.colors.surface)
             ) {
-                Surface(
-                    modifier = Modifier.size(130.dp),
-                    shape = RoundedCornerShape(12.dp),
-                    color = MaterialTheme.colors.surface.copy(
-                        alpha = 0.2f
-                    )
-                ) {
-//                    val image = rememberCoilPainter(
-//                        request = empData.avatar,
-//                        fadeIn = true
-//                    )
-//                    Image(
-//                        painter = image,
-//                        contentDescription = null,
-//                        modifier = Modifier
-//                            .height(100.dp)
-//                            .clip(shape = RoundedCornerShape(12.dp)),
-//                        contentScale = ContentScale.Crop
-//                    )
-                }
                 Column(
                     modifier = Modifier
-                        .padding(start = 12.dp)
+                        .padding(start = 16.dp)
                         .align(Alignment.CenterVertically)
                 ) {
                     Text(
@@ -165,4 +147,29 @@ class PaginatedFragment2 :
         }
     }
 
+    @Composable
+    @Preview(
+        device = Devices.PIXEL_4,
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        locale = "es"
+    )
+    @Preview(
+        device = Devices.PIXEL_4,
+        uiMode = Configuration.UI_MODE_NIGHT_NO,
+        locale = "es"
+    )
+    private fun PaginatedPreview() {
+        AstroPayTheme {
+            EmployeeItem(
+                empData = User(
+                    id = 1,
+                    email = "ideandreis64@gmail.com",
+                    first_name = "Ignacio",
+                    last_name = "Deandreis",
+                    avatar = "avatar"
+                ),
+                onClick = {}
+            )
+        }
+    }
 }
